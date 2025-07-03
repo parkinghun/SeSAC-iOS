77f16af6e3f93e10cdfbe900366d61686c8200e0
Sunghun Park <dfstem9@gmail.com>
2025-07-03 15:21:58 +0900

    신조어 탐색기 - 세팅

---
+++ NeologismFinder/NeologismFinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NeologismFinder
+//
+//  Created by 박성훈 on 7/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
