630b484d7ebe417f5d53909f27f98e2ea5942b8b
Sunghun Park <dfstem9@gmail.com>
2025-07-12 22:03:16 +0900

    Travel - 광고 데이터 추가

---
+++ TravelProject/TravelProject/Travel/AdInfo.swift
@@ -0,0 +1,18 @@
+//
+//  AdInfo.swift
+//  TravelProject
+//
+//  Created by 박성훈 on 7/12/25.
+//
+
+import Foundation
+
+struct AdInfo {
+    let ads: [Ad] = [
+        Ad(message: "하와이 여행을 가고싶다면?\n수업이 있는데 가실 생각은 아니시죠?", backgroundColor: .systemPink),
+        Ad(message: "도쿄 여행 예약은?\nXCode로 직업 예약앱을 만들면 되겠네요!", backgroundColor: .green),
+        Ad(message: "유럽 여행을 가고싶다면?\n저도 가고싶어요!", backgroundColor: .orange),
+        Ad(message: "올 여름 여행지는 어디?\n새싹으로 오세요!", backgroundColor: .cyan)
+        
+    ]
+}
