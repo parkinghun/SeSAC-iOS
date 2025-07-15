0aee8406565c298862f2d6d57f4349889512dbba
Sunghun Park <dfstem9@gmail.com>
2025-07-15 23:11:14 +0900

    Popular - Segment를 통한 데이터 분기 구현

--- TravelProject/TravelProject/Tab/Popular/PopularTableViewController.swift
+++ TravelProject/TravelProject/Tab/Popular/PopularTableViewController.swift
@@ -7,18 +7,37 @@
 
 import UIKit
 
-class PopularViewController: UIViewController {
+enum MenuSection: CaseIterable {
+    case all
+    case domestic
+    case overseas
+    
+    var title: String {
+        switch self {
+        case .all: return "모두"
+        case .domestic: return "국내"
+        case .overseas: return "해외"
+        }
+    }
+}
+
+final class PopularViewController: UIViewController {
     
     let cityList = CityInfo().city
+    var filteredCityList: [City] = []
     
     @IBOutlet var tableView: UITableView!
     @IBOutlet var citySegment: UISegmentedControl!
+    @IBOutlet var textFieldBgView: UIView!
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var searchButton: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureNavigationTitle()
         setTableViewDelegate()
+        setupSegmentControl()
     }
     
     private func configureNavigationTitle() {
@@ -31,16 +50,51 @@ class PopularViewController: UIViewController {
         tableView.showsVerticalScrollIndicator = false
     }
     
+    private func setupSegmentControl() {
+        citySegment.removeAllSegments()
+        
+        MenuSection.allCases.enumerated().forEach { (index, section) in
+            citySegment.insertSegment(withTitle: section.title, at: index, animated: true)
+        }
+
+        citySegment.selectedSegmentIndex = 0
+        filteredCityList = cityList
+    }
+    
     @IBAction func citySegmentTapped(_ sender: UISegmentedControl) {
         print(#function, sender.selectedSegmentIndex)
+        
+        switch sender.selectedSegmentIndex {
+        case 0:
+            filteredCityList = cityList
+            tableView.reloadData()
+        case 1:
+            filteredCityList = cityList.filter { $0.domesticTravel }
+            tableView.reloadData()
+        case 2:
+            filteredCityList = cityList.filter { !$0.domesticTravel}
+            tableView.reloadData()
+        default: break
+        }
     }
+    
+    @IBAction func searchTextFieldDidEndOnExit(_ sender: UITextField) {
+        // 엔터눌렀을 때
+        print(#function)
+    }
+    
+    
+    @IBAction func searchTextFieldEditingChanged(_ sender: UITextField) {
+        print(#function)
+    }
+    
     
 }
 
 // MARK: - UITableViewDataSource
 extension PopularViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cityList.count
+        return filteredCityList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -49,7 +103,7 @@ extension PopularViewController: UITableViewDataSource {
         
         guard let cell = tableView.dequeueReusableCell(withIdentifier: PopularTableViewCell.id, for: indexPath) as? PopularTableViewCell else { return UITableViewCell() }
         
-        cell.configure(data: cityList[indexPath.row])
+        cell.configure(data: filteredCityList[indexPath.row])
         return cell
     }
 }
@@ -57,7 +111,7 @@ extension PopularViewController: UITableViewDataSource {
 // MARK: - UITableViewDelegate
 extension PopularViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200
+        return 180
     }
     
 
