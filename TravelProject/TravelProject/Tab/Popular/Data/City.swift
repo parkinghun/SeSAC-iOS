2e42ff07d6b6f29ff23e3f5b9cfad756c65b397b
Sunghun Park <dfstem9@gmail.com>
2025-07-15 18:03:55 +0900

    Popular - CityInfo 데이터 추가 및 City 구조체 구현

---
+++ TravelProject/TravelProject/Tab/Popular/Data/City.swift
@@ -0,0 +1,24 @@
+//
+//  City.swift
+//  TravelProject
+//
+//  Created by 박성훈 on 7/15/25.
+//
+
+import Foundation
+
+struct City {
+    let cityName: String
+    let cityEnglishName: String
+    let cityExplain: String
+    let cityImage: String
+    let domesticTravel: Bool
+    
+    init(city_name cityName: String, city_english_name cityEnglishName: String, city_explain cityExplain: String, city_image cityImage: String, domestic_travel domesticTravel: Bool) {
+        self.cityName = cityName
+        self.cityEnglishName = cityEnglishName
+        self.cityExplain = cityExplain
+        self.cityImage = cityImage
+        self.domesticTravel = domesticTravel
+    }
+}
