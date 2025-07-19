a423483a0d21bfc8054036152e9e68a6bf87a03e
Sunghun Park <dfstem9@gmail.com>
2025-07-19 19:06:25 +0900

    Chat 탭바 연결 및 TravelTalk VC - Stroyboard 연결

---
+++ TravelProject/TravelProject/Tab/Chat/ViewController/TravelTalkViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TravelTalkViewController.swift
+//  TravelProject
+//
+//  Created by 박성훈 on 7/19/25.
+//
+
+import UIKit
+
+class TravelTalkViewController: UIViewController {
+
+    static let id = String(describing: TravelTalkViewController.self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+}
