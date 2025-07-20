e3fe950844f07d632c0a312525a831e76c1483e7
Sunghun Park <dfstem9@gmail.com>
2025-07-20 10:35:43 +0900

    feat: Chat - TravleTalkViewController, Cell 연결

---
+++ TravelProject/TravelProject/Tab/Chat/View/TravelTalkCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  TravelTalkCollectionViewCell.swift
+//  TravelProject
+//
+//  Created by 박성훈 on 7/19/25.
+//
+
+import UIKit
+
+class TravelTalkCollectionViewCell: UICollectionViewCell {
+
+    static let id = String(describing: TravelTalkCollectionViewCell.self)
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+
+}
--- TravelProject/TravelProject/Tab/Chat/ViewController/TravelTalkViewController.swift
+++ TravelProject/TravelProject/Tab/Chat/ViewController/TravelTalkViewController.swift
@@ -7,15 +7,47 @@
 
 import UIKit
 
-class TravelTalkViewController: UIViewController {
-
-    static let id = String(describing: TravelTalkViewController.self)
+final class TravelTalkViewController: UIViewController {
     
+    @IBOutlet var collectionView: UICollectionView!
+    static let id = String(describing: TravelTalkViewController.self)
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        
+        setupNavigation()
+        setupCollectionView()
     }
     
+    private func setupNavigation() {
+        navigationItem.title = "TRAVEL TALK"
+    }
 
+    private func setupCollectionView() {
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        let nib = UINib(nibName: "TravelTalk", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: TravelTalkCollectionViewCell.id)
+    }
+    
+    private func setLayout() {
+        
+    }
+}
+
+extension TravelTalkViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: TravelTalkCollectionViewCell.id, for: indexPath) as? TravelTalkCollectionViewCell else { return UICollectionViewCell() }
+        
+        return cell
+    }
+}
+
+extension TravelTalkViewController: UICollectionViewDelegate {
+    
 }
