e576528ee8451bdb52db4849f2fc0383f5697068
Sunghun Park <dfstem9@gmail.com>
2025-07-21 15:13:29 +0900

    First Commit - 빌드 세팅

---
+++ JACKFLIXWithCodeBase/JACKFLIXWithCodeBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JACKFLIXWithCodeBase
+//
+//  Created by 박성훈 on 7/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
