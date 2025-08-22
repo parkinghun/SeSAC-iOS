56aed597408d170c79b30f8412196d3ae898b7e7
Sunghun Park <dfstem9@gmail.com>
2025-08-22 09:23:33 +0900

    First Commit - Project Setting

---
+++ RxTamagochi/RxTamagochi/UserDefaultsManager.swift
@@ -0,0 +1,33 @@
+//
+//  UserDefaultsManager.swift
+//  Tamagotchi
+//
+//  Created by 박성훈 on 7/9/25.
+//
+
+import Foundation
+
+// TODO: - PropertyWrapper를 이용해 리팩토링하기
+final class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    private let defaults = UserDefaults.standard
+    
+    private enum Key {
+        static let tamagochi = "tamagochi"
+    }
+    
+    private init() {}
+    
+    func saveTamagochi(_ tamagochi: Tamagochi) {
+        let encoder = JSONEncoder()
+        if let encoded = try? encoder.encode(tamagochi) {
+            defaults.set(encoded, forKey: Key.tamagochi)
+        }
+    }
+
+    func loadTamagochi() -> Tamagochi? {
+        guard let data = defaults.data(forKey: Key.tamagochi) else { return nil }
+        let decoder = JSONDecoder()
+        return try? decoder.decode(Tamagochi.self, from: data)
+    }
+}
